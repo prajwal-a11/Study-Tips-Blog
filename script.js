1f6b91fb80ba81c40bebb0e069b051ffe4142692
prajwal-a11 <prajwal.hosamani@s.amity.edu>
2025-05-26 22:30:37 -0400

    Script file for study tips | script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function saveTasks() 
+{
+const tasks = Array. from(document.querySelectorAll('#taskList li')).map(li =>li.textContent);
+localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+function loadTasks() {
+const tasks = JSON.parse(localStorage.getItem('tasks') || "[]");
+tasks. forEach(task =>{
+const li = document.createElement('li');
+li.textContent = task;
+document.getElementById('taskList').ap 
+pendChild(li);
+});
+}
+window.onload = loadTasks;
+document.getElementById('taskList').ap 
+pendChild(li); 
+saveTasks();
